905cb69696927a9360757751789bf7290245a73d
Prashanth <yvprashanth@gmail.com>
2017-08-06 23:46:22 -0700

    calculated totalAmount based on bill and amount selected

--- TipsyCow/ViewController.swift
+++ TipsyCow/ViewController.swift
@@ -15,11 +15,12 @@ class ViewController: UIViewController {
     }
    
     @IBAction func tipEnteredAction(_ sender: UITextField){
-
-        display!.text = String(Double(tipTextField.text!)! * Double(tipAmountSelected.titleForSegment(at: tipAmountSelected.selectedSegmentIndex)!)!)
+        let tipAmount = (tipAmountSelected.titleForSegment(at: tipAmountSelected.selectedSegmentIndex)!)
+        let tipAmountWithoutPercentage = tipAmount.characters.index(of: "%")
+        let tipAmountWithoutPercentageDigit = String(tipAmount.characters.prefix(upTo: tipAmountWithoutPercentage!))
+        display!.text = String(Double(tipTextField.text!)! * Double(tipAmountWithoutPercentageDigit)!)
     }
     
     @IBOutlet weak var tipAmountSelected: UISegmentedControl!
     @IBOutlet weak var tipTextField: UITextField!
 }
-
